90f9b40eb40a1f257eb788d832ed9824e91dcdfc
ludan95 <Mludan95@gmail.com>
2022-03-31 22:14:04 -0600

    altaUsuarioArtesano

---
+++ js/archivo.js
@@ -0,0 +1,18 @@
+window.sr= ScrollReveal();
+
+sr.reveal('.navbar',{
+    duration: 2000,
+    origin: 'bottom'
+});
+
+sr.reveal('.header-content-left',{
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+
+sr.reveal('.header-content-right',{
+    duration: 2500,
+    origin: 'right',
+    distance: '300px'
+});
